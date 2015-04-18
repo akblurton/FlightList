e3f4c06203069793f89764eea8289b7b78bea3cb
Ashley Blurton <ashley.blurton@thehivegroup.net>
2015-04-18 14:24:50 +0100

    Login functionality

--- js/directives.js
+++ js/directives.js
@@ -1,12 +1,65 @@
-/* global flightList */
-flightList.directive("loginPanel", [function(){
+/* global flightList, config */
+flightList.directive("loginPanel", ["$http", function($http){
 
 	return {
 		scope: {
 			"onAuth" : "="
 		},
-		controller: function($scope, $element, $attrs, $transclude) {
+		controller: function($scope, $element) {
+			$scope.user = {
+				"email" : "",
+				"password" : ""
+			};
 
+			function login() {
+				window.localStorage.setItem("flightlist:session", "1");
+				$element.addClass("off");
+
+				// Call authentication method
+				if($scope.onAuth && $scope.onAuth.call) {
+					$scope.onAuth();
+				}
+			}
+
+			// Check if user is already logged in
+			var status = window.localStorage.getItem("flightlist:session");
+			if(status == "1") {
+				login();
+			}
+
+			$scope.attempt = function() {
+				// Wait until user completes form
+				if(!$scope.user.email || !$scope.user.password) {
+					return false;
+				}
+
+				$scope.error = null;
+
+				// Animate submit button
+				$element.addClass("working");
+				// Disable form
+				$element.find("input, button").attr("disabled", "disabled");
+
+				// Send login data to API
+				$http[config().api.method](config().api.login, $scope.user)
+					.success(function(data) {
+						if(!data || !data.success) {
+							$scope.error = "Invalid login credentials provided";
+						}
+						else {
+							login();
+						}
+					})
+					.error(function() {
+						$scope.error = "Could not connect to login server";
+					})
+					.finally(function() {
+						// Reset form after every API call
+						$scope.user.password = "";
+						$element.removeClass("working");
+						$element.find("input, button").removeAttr("disabled").eq(1).focus();
+					});
+			};
 		},
 		restrict: "E",
 		// template: '',
--- js/flight.js
+++ js/flight.js
@@ -1,9 +1,22 @@
 /* global angular */
+/* exported config */
+
+// Config object, use $.extend to prevent modification
+function config() {
+	return $.extend({}, {
+		"api" : {
+			"login" : "api/login.json",
+			"method" : "get"
+		}
+	});
+}
+
+
 var flightList = angular.module("FlightList", []);
 
 // Login Controller
 flightList.controller("LoginCtrl", ["$scope", function($scope) {
     $scope.showList = function() {
         
-    }
+    };
 }]);
